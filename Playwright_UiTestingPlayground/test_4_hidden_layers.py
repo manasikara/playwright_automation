e6b3b939a5e8182422e2db3a2a0530efaae39d24
slawomir_tatinger <59393759+manasikara@users.noreply.github.com>
2023-01-18 15:28:35 +0100

    test_4

--- Playwright_UiTestingPlayground/test_4_hidden_layers.py
+++ Playwright_UiTestingPlayground/test_4_hidden_layers.py
@@ -1,4 +1,5 @@
 #http://uitestingplayground.com/hiddenlayers
+#headless test
 
 from playwright.sync_api import sync_playwright
 
@@ -6,4 +7,8 @@ with sync_playwright() as p:
     browser = p.chromium.launch()
     page = browser.new_page()
     page.goto("http://uitestingplayground.com")
-    page.click
+    page.click('text=Hidden Layers')
+    page.click('#greenButton')
+    page.click('#blueButton')
+    print("you're an amazing automation tester!")
+    
