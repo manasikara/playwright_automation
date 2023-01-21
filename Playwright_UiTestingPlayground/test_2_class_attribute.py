284653eb11580f34ca8474fb7eb3d8e72b45f917
manasikara <slawektatinger@me.com>
2023-01-21 22:16:50 +0100

    test update

--- Playwright_UiTestingPlayground/test_2_class_attribute.py
+++ Playwright_UiTestingPlayground/test_2_class_attribute.py
@@ -1,10 +1,11 @@
 #learning automation using a pytest
 #http://uitestingplayground.com/classattr
-#https://playwright.dev/python/docs/test-runners
+#pytest usage --> https://playwright.dev/python/docs/test-runners
 
-def test_class_attribute(page)
+def test_class_attribute(page):
     page.goto("http://uitestingplayground.com/")
     page.click('//*[@id="overview"]/div/div[1]/div[2]/h3/a')
-    page.click(":button('.btn class1 btn-success btn-test')")
-    
+    page.locator('/html/body/section/div/button[1]')
+    page.click('text=ok')
+
     
