9fb0c6fef7fd882f24dce465630a5efcf88dead3
manasikara <slawektatinger@me.com>
2023-03-22 17:59:36 +0100

    'sklep' test updated

--- skleptest.pl/test_sklep.py
+++ skleptest.pl/test_sklep.py
@@ -1,13 +1,39 @@
 from playwright.sync_api import sync_playwright
-import time
-
 def run(playwright):
-    chromium = playwright.chromium
-    browser = chromium.launch(headless=False,slomo=400)
-    page = browser.new_page()
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    # Open new page
+    page = context.new_page()
+    # Go to https://skleptest.pl/
     page.goto("https://skleptest.pl/")
-    # other actions...
+    # Click text=skleptestarmy@gmail.com
+    page.click("text=skleptestarmy@gmail.com")
+    # assert page.url == "https://skleptest.pl/#"
+    # Click [placeholder="Search ..."]
+    page.click("[placeholder=\"Search ...\"]")
+    # Fill [placeholder="Search ..."]
+    page.fill("[placeholder=\"Search ...\"]", "dress")
+    # Press Enter
+    page.press("[placeholder=\"Search ...\"]", "Enter")
+    # assert page.url == "https://skleptest.pl/?s=dress"
+    # Click text=Account
+    page.click("text=Account")
+    # assert page.url == "https://skleptest.pl/my-account/"
+    # Click text=My Cart - zł 0
+    page.click("text=My Cart - zł 0")
+    # assert page.url == "https://skleptest.pl/cart/"
+    # Click text=Return to shop
+    page.click("text=Return to shop")
+    # Images are not being loaded properly!!!
+    # assert page.url == "https://skleptest.pl/shop/"
+    # Click text=Generic Shop
+    page.click("text=Generic Shop")
+    # assert page.url == "https://skleptest.pl/"
+    # ---------------------
+    context.close()
     browser.close()
-
 with sync_playwright() as playwright:
     run(playwright)
+
+
+# TOP BAR TESTED - TBC...
