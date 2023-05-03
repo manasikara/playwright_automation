123d3ab5c1a65d99c67a66afab1f46d4bc8e8cb9
manasikara <slawektatinger@me.com>
2023-05-03 22:16:48 +0200

    '2) JAVA SCRIPT ALERT' next in queue

---
+++ Practice_form/test_p_form.py
@@ -0,0 +1,31 @@
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False, slow_mo=600)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("http://www.seleniumframework.com/Practiceform/")
+    with page.expect_popup() as page1_info:
+        page.get_by_role("button", name="New Browser Window").click()
+    page1 = page1_info.value
+    page1.close()
+    with page.expect_popup() as page2_info:
+        page.get_by_role("button", name="New Message Window").click()
+    page2 = page2_info.value
+    page2.close()
+    with page.expect_popup() as page3_info:
+        page.get_by_role("button", name="New Browser Tab").click()
+    page3 = page3_info.value
+    page3.close()
+    page.get_by_text("Find me I have nothing in me!!").click()
+    page.get_by_text("I will have random ID").click()
+
+    print('Done!')
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
