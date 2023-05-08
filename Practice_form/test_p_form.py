2e6b437d1a270d61298652f269fa45a851119067
manasikara <slawektatinger@me.com>
2023-05-08 20:32:28 +0200

    error corrected

--- Practice_form/test_p_form.py
+++ Practice_form/test_p_form.py
@@ -1,36 +1,37 @@
 from playwright.sync_api import Playwright, sync_playwright, expect
 import time
 
-def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False, slow_mo=600)
-    context = browser.new_context()
-    page = context.new_page()
-    page.goto("http://www.seleniumframework.com/Practiceform/")
-    with page.expect_popup() as page1_info:
-        page.get_by_role("button", name="New Browser Window").click()
-    page1 = page1_info.value
-    page1.close()
-    with page.expect_popup() as page2_info:
-        page.get_by_role("button", name="New Message Window").click()
-    page2 = page2_info.value
-    page2.close()
-    with page.expect_popup() as page3_info:
-        page.get_by_role("button", name="New Browser Tab").click()
-    page3 = page3_info.value
-    page3.close()
-    page.get_by_text("Find me I have nothing in me!!").click()
-    page.get_by_text("I will have random ID").click()
+def test_p_form():
+    def run(playwright: Playwright) -> None:
+        browser = playwright.chromium.launch(headless=False, slow_mo=600)
+        context = browser.new_context()
+        page = context.new_page()
+        page.goto("http://www.seleniumframework.com/Practiceform/")
+        with page.expect_popup() as page1_info:
+            page.get_by_role("button", name="New Browser Window").click()
+        page1 = page1_info.value
+        page1.close()
+        with page.expect_popup() as page2_info:
+            page.get_by_role("button", name="New Message Window").click()
+        page2 = page2_info.value
+        page2.close()
+        with page.expect_popup() as page3_info:
+            page.get_by_role("button", name="New Browser Tab").click()
+        page3 = page3_info.value
+        page3.close()
+        page.get_by_text("Find me I have nothing in me!!").click()
+        page.get_by_text("I will have random ID").click()
 
-    page.click("text=Alert Box")
-    page.get_by_role("button", name="Alert Box").click()
-
-
-
-    print('Done!')
-    # ---------------------
-    context.close()
-    browser.close()
+        page.click("text=Alert Box")
+        page.get_by_role("button", name="Alert Box").click()
 
 
-with sync_playwright() as playwright:
-    run(playwright)
+
+        print('Done!')
+        # ---------------------
+        context.close()
+        browser.close()
+
+
+    with sync_playwright() as playwright:
+        run(playwright)
--- skleptest.pl/test_sklep1.py
+++ skleptest.pl/test_sklep1.py
@@ -1,87 +1,100 @@
-# pytest - sample testing drop-down list
+# testing drop-down list
 
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Playwright, sync_playwright, expect
 import re
 import time
 
-def test_sklep1():
-    def run(page: Page):
-            page.goto("https://skleptest.pl")
-            
-            # Hover over 'catergries' <-- a misspelling BUG here!!! Should be a 'categories' instead.
-            header_menu = page.locator("a.dropdown-toggle", has_text="catergries")
-            header_menu.hover()
-            
-            # Click "All"
-            page.click('text=all')
-            # Click text=Generic Shop
-            page.click("text=Generic Shop")
-            # Hover over 'catergries'
-            header_menu = page.locator("a.dropdown-toggle", has_text="catergries")
-            header_menu.hover()
-            
-            page.click('text=shirts')
-            # Click text=Generic Shop
-            page.click("text=Generic Shop")
-            # Hover over 'catergries'
-            header_menu = page.locator("a.dropdown-toggle", has_text="catergries")
-            header_menu.hover()
-            
-            page.click('text=featured')
-            # Click text=Generic Shop
-            page.click("text=Generic Shop")
-            # Hover over 'catergries'
-            header_menu = page.locator("a.dropdown-toggle", has_text="catergries")
-            header_menu.hover()
-            
-            page.click('text=trends')
-            # Click text=Generic Shop
-            page.click("text=Generic Shop")
-            # Hover over 'catergries'
-            header_menu = page.locator("a.dropdown-toggle", has_text="catergries")
-            header_menu.hover()
-            
-            page.click('text=scarfs')
-            # Click text=Generic Shop
-            page.click("text=Generic Shop")
-            # Hover over 'catergries'
-            header_menu = page.locator("a.dropdown-toggle", has_text="catergries")
-            header_menu.hover()
-            
-            page.click('text=shoes')
-            # Click text=Generic Shop
-            page.click("text=Generic Shop")
-            # Hover over 'catergries'
-            header_menu = page.locator("a.dropdown-toggle", has_text="catergries")
-            header_menu.hover()
-            
-            page.click('text=tops')
-            # Click text=Generic Shop
-            page.click("text=Generic Shop")
-            # Hover over 'catergries'
-            header_menu = page.locator("a.dropdown-toggle", has_text="catergries")
-            header_menu.hover()
-            
-            page.click('text=blouse')
-            # Click text=Generic Shop
-            page.click("text=Generic Shop")
-            # Hover over 'catergries'
-            header_menu = page.locator("a.dropdown-toggle", has_text="catergries")
-            header_menu.hover()
-            
-            page.click('text=jeans')
-            # Click text=Generic Shop
-            page.click("text=Generic Shop")   
-            # Hover over 'catergries'
-            header_menu = page.locator("a.dropdown-toggle", has_text="catergries")
-            header_menu.hover()
-            
-            page.click('text=dresses')
-            # Click text=Generic Shop
-            page.click("text=Generic Shop")
-            
-            
-            
-            print('Thanks for shopping with us!')
-            page.close()
-    
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False,)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://skleptest.pl")
+    # Hover over 'catergries' <-- a misspelling BUG here!!! Should be a 'categories' instead.
+    header_menu = page.locator("a.dropdown-toggle", has_text="catergries")
+    header_menu.hover()
+
+    # Click "All"
+    page.click('text=all')
+    # Click text=Generic Shop
+    page.click("text=Generic Shop")
+    # Hover over 'catergries'
+    header_menu = page.locator("a.dropdown-toggle", has_text="catergries")
+    header_menu.hover()
+
+    page.click('text=shirts')
+    # Click text=Generic Shop
+    page.click("text=Generic Shop")
+    # Hover over 'catergries'
+    header_menu = page.locator("a.dropdown-toggle", has_text="catergries")
+    header_menu.hover()
+
+    page.click('text=featured')
+    # Click text=Generic Shop
+    page.click("text=Generic Shop")
+    # Hover over 'catergries'
+    header_menu = page.locator("a.dropdown-toggle", has_text="catergries")
+    header_menu.hover()
+
+    page.click('text=trends')
+    # Click text=Generic Shop
+    page.click("text=Generic Shop")
+    # Hover over 'catergries'
+    header_menu = page.locator("a.dropdown-toggle", has_text="catergries")
+    header_menu.hover()
+
+    page.click('text=scarfs')
+    # Click text=Generic Shop
+    page.click("text=Generic Shop")
+    # Hover over 'catergries'
+    header_menu = page.locator("a.dropdown-toggle", has_text="catergries")
+    header_menu.hover()
+
+    page.click('text=shoes')
+    # Click text=Generic Shop
+    page.click("text=Generic Shop")
+    # Hover over 'catergries'
+    header_menu = page.locator("a.dropdown-toggle", has_text="catergries")
+    header_menu.hover()
+
+    page.click('text=tops')
+    # Click text=Generic Shop
+    page.click("text=Generic Shop")
+    # Hover over 'catergries'
+    header_menu = page.locator("a.dropdown-toggle", has_text="catergries")
+    header_menu.hover()
+
+    page.click('text=blouse')
+    # Click text=Generic Shop
+    page.click("text=Generic Shop")
+    # Hover over 'catergries'
+    header_menu = page.locator("a.dropdown-toggle", has_text="catergries")
+    header_menu.hover()
+
+    page.click('text=jeans')
+    # Click text=Generic Shop
+    page.click("text=Generic Shop")
+    # Hover over 'catergries'
+    header_menu = page.locator("a.dropdown-toggle", has_text="catergries")
+    header_menu.hover()
+
+    page.click('text=dresses')
+    # Click text=Generic Shop
+    page.click("text=Generic Shop")
+
+    print('Thanks for shopping with us!')
+    page.close()
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
+
+
+
+
+
+
