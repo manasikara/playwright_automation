544dd029ea6ac3cc781e753d98ff054e6a141d9a
manasikara <slawektatinger@me.com>
2023-05-08 21:56:11 +0200

    log-in testing

--- Demobank/test_login.py
+++ Demobank/test_login.py
@@ -1,28 +1,29 @@
 from playwright.sync_api import Playwright, sync_playwright, expect
 
+def test_login():
+    def run(playwright: Playwright) -> None:
+        browser = playwright.chromium.launch(headless=False, slow_mo=500)
+        context = browser.new_context()
+        page = context.new_page()
+        page.goto("https://demobank.jaktestowac.pl/")
+        page.goto("https://demobank.jaktestowac.pl/logowanie_etap_1.html")
+        page.locator("#login_id").click()
+        page.locator("#login_id").fill("12345678")
+        page.locator("#login_id").press("Enter")
+        page.locator("#login_password").click()
+        page.locator("#login_password").fill("12345678")
+        page.locator("#login_password").press("Enter")
 
-def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    page = context.new_page()
-    page.goto("https://demobank.jaktestowac.pl/")
-    page.goto("https://demobank.jaktestowac.pl/logowanie_etap_1.html")
-    page.locator("#login_id").click()
-    page.locator("#login_id").fill("12345678")
-    page.locator("#login_id").press("Enter")
-    page.locator("#login_password").click()
-    page.locator("#login_password").fill("12345678")
-    page.locator("#login_password").press("Enter")
+        # ---------------------
 
-    # ---------------------
-    context.close()
-    browser.close()
-
-
-with sync_playwright() as playwright:
-    run(playwright)
+        context.close()
+        browser.close()
 
 
+    with sync_playwright() as playwright:
+        run(playwright)
+
+    print('Done!')
     
     
     
