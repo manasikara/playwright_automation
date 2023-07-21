495ab1d13ad5f209591a2ddfb0a189dd0f776b10
slawomir_tatinger <59393759+manasikara@users.noreply.github.com>
2023-07-21 10:27:36 +0200

    test added

--- antycaptcha/test_dropdown.py
+++ antycaptcha/test_dropdown.py
@@ -7,11 +7,10 @@ def test_three_buttons():
         browser = playwright.chromium.launch(headless=False, slow_mo=800)
         context = browser.new_context()
         page = context.new_page()
-        page.goto("https://antycaptcha.amberteam.pl/exercises/exercise2?seed=9af17c5b-0d85-4922-9b1d-ff1abd7e170b")
-        page.get_by_role("textbox").click()
-        page.get_by_role("textbox").fill("Cut my coach.")
-        page.get_by_role("button", name="B1").click()
+        page.goto("https://antycaptcha.amberteam.pl/exercises/exercise3?seed=89c22dde-2af2-4041-af81-185afd93dd85")
+        page.get_by_role("combobox").select_option("v6")
         page.get_by_role("button", name="Check solution").click()
+        
 
     # ---------------------
 
