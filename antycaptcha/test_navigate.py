4c4f550978c9be0dd3b26dcb7fa71a7c2ff15146
slawomir_tatinger <59393759+manasikara@users.noreply.github.com>
2023-07-23 10:52:01 +0200

    don't know how to solve this one yet

---
+++ antycaptcha/test_navigate.py
@@ -0,0 +1,23 @@
+# testing resources --> https://antycaptcha.amberteam.pl
+
+from playwright.sync_api import Playwright, sync_playwright, expect
+import time
+
+def test_ID():
+    def run(playwright: Playwright) -> None:
+        browser = playwright.chromium.launch(headless=False, slow_mo=700)
+        context = browser.new_context()
+        page = context.new_page()
+        page.goto("http://antycaptcha.amberteam.pl/stf/3-2-1/")
+        #  ? ? ? ? ? ?? ? ? ? <--- no clue how to do this
+        time.sleep(2)
+
+        # ---------------------
+        context.close()
+        browser.close()
+
+
+    with sync_playwright() as playwright:
+        run(playwright)
+
+# need to work on this one!!!!!!!
